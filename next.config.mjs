3d36e954c401233ec10c0b284c175cedf5d25dc6
YouLikeCats <thisisbryanchan@gmail.com>
2024-08-06 16:27:49 -0400

    changed footer layout

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,23 @@
-import nextMDX from "@next/mdx";
-import rehypeHighlight from "rehype-highlight";
-import rehypeKatex from 'rehype-katex'
-import remarkMath from 'remark-math'
+import pkg from './package.json' assert { type: 'json' };
+import { execSync } from 'child_process';
+
+// Starts a command line process to get the git hash
+const commitHash = execSync('git log --pretty=format:"%h" -n1')
+  .toString()
+  .trim();
+
+const fullCommitHash = execSync('git rev-parse HEAD')
+  .toString()
+  .trim();
 
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-      },
-    ],
-  },
-}
+  // Other configs
+  env: {
+    // Add the package.json version and git hash to the environment
+    APP_VERSION: pkg.version,
+    COMMIT_HASH: commitHash,
+    FULL_COMMIT_HASH: fullCommitHash,
+  }
+};
 
-const withMDX = nextMDX({
-  options: {
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeHighlight, rehypeKatex],
-  },
-});
-
-export default withMDX(nextConfig);
+export default nextConfig;
