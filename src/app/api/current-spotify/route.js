06ea4bcb27b227368183f7f2af062bacbe059c30
brrryry <thisisbryanchan@gmail.com>
2024-08-13 09:52:52 -0400

    changed spotify api integration to be rate-limited on server side cache instead of client side cache

--- src/app/api/current-spotify/route.js
+++ src/app/api/current-spotify/route.js
@@ -1,30 +1,50 @@
 import { currentlyPlayingSong } from "@/lib/spotify";
 
-export const revalidate = 60
+const apiCalls = new Map();
+
+const timeLimit = 10000; // 10 seconds time limit between new 
+
 
 export async function GET(req) {
+    const currentTime = Date.now();
+    const cachedSong = apiCalls.get("last-song");
+    const cachedTime = apiCalls.get("last-time");
 
+    // Check if there is a cached response and if it's still valid
+    if (cachedTime && currentTime - cachedTime < timeLimit) {
+        return new Response(JSON.stringify(cachedSong), { status: 200 });
+    }
+
+    // Fetch the current song data
     const response = await currentlyPlayingSong();
 
+    apiCalls.set("last-time", currentTime); //regardless of response, assert cache time
+
     if (response.status === 204 || response.status > 400) {
-      return Response.json({ isPlaying: false });
+        return new Response(JSON.stringify({ isPlaying: false }), { status: 200 });
     }
-  
+
     const song = await response.json();
-  
+
     if (song.item === null) {
-      return res.status(200).json({ isPlaying: false });
+
+        return new Response(JSON.stringify({ isPlaying: false }), { status: 200 });
     }
-  
+
     const isPlaying = song.is_playing;
     const title = song.item.name;
     const artist = song.item.artists.map((_artist) => _artist.name).join(", ");
     const songUrl = song.item.external_urls.spotify;
 
-    return Response.json({
-      artist,
-      isPlaying,
-      songUrl,
-      title,
-    });
+    const res = {
+        artist,
+        isPlaying,
+        songUrl,
+        title
+    };
+
+    // Cache the new response and timestamp
+    apiCalls.set("last-song", res);
+
+    return new Response(JSON.stringify(res), { status: 200 });
 }
