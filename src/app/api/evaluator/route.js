1ef28bb1880e86ee7d2de07c4c5942824df8e4ee
brrryry <thisisbryanchan@gmail.com>
2024-12-24 00:58:11 -0500

    adding evaluator api things (for git-aura)

--- src/app/api/evaluator/route.js
+++ src/app/api/evaluator/route.js
@@ -2,28 +2,35 @@ import { connectToDatabase } from "@/lib/mongo";
 import { ObjectId } from "mongodb";
 
 export async function GET(req) {
-    try {
-        const { db } = await connectToDatabase();
-        const users = await db.collection("users").find({}).toArray();
+  try {
+    const { db } = await connectToDatabase();
+    const users = await db.collection("users").find({}).toArray();
 
-        return new Response(JSON.stringify(users), { status: 200 });
-    } catch (e) {
-        return new Response(JSON.stringify({ error: e.message }), { status: 500 });
-    }
+    return new Response(JSON.stringify(users), { status: 200 });
+  } catch (e) {
+    return new Response(JSON.stringify({ error: e.message }), { status: 500 });
+  }
 }
 
 export async function POST(req) {
-    try {
-        const { db } = await connectToDatabase();
-        const {user_id, aura} = req.body;
-        const user = await db.collection("users").findOne({ user_id });
+  try {
+    const { db } = await connectToDatabase();
+    const { user_id, aura } = req.body;
+    const user = await db.collection("users").findOne({ user_id });
 
-        if(!user) {
-            return new Response(JSON.stringify({ error: "User not found" }), { status: 404 });
-        }
-
-        await db.collection("users").updateOne({ _id: new ObjectId(user_id) }, { $set: { aura: Number(aura) } });
-    } catch(e) {
-        return new Response(JSON.stringify({ error: e.message }), { status: 500 });
+    if (!user) {
+      return new Response(JSON.stringify({ error: "User not found" }), {
+        status: 404,
+      });
     }
+
+    await db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(user_id) },
+        { $set: { aura: Number(aura) } },
+      );
+  } catch (e) {
+    return new Response(JSON.stringify({ error: e.message }), { status: 500 });
+  }
 }
