60a10ad69d0e3765a1b461c44b3c107628ed6306
brrryry <thisisbryanchan@gmail.com>
2025-01-25 12:30:00 -0500

    building more of the mysql implementation + writing japan blog

--- src/lib/comments.js
+++ src/lib/comments.js
@@ -1,8 +1,10 @@
 import mysql from 'mysql';
-import { sqlSettings } from './sql.js';
+import { createCommentsTable, sqlSettings } from './sql.js';
 
 export const createComment = (comment) => {
     let connection = mysql.createConnection(sqlSettings());
+    createCommentsTable(connection);
+
     connection.connect();
 
     //check comment parameters
@@ -67,6 +69,101 @@ export const createComment = (comment) => {
         (err, result) => {
             if (err) throw err;
             console.log('Comment created');
+        }
+    );
+
+    connection.end();
+
+    return true;
+}
+
+export const getComments = (postid) => {
+    if (!postid) {
+        throw new Error('Invalid comment parameters');
+    }
+
+    let connection = mysql.createConnection(sqlSettings());
+    createCommentsTable(connection);
+
+    connection.connect();
+
+    let comments = [];
+    let totalComments = 0;
+
+    connection.query(
+        'SELECT * FROM comments WHERE postid = ?',
+        [postid],
+        (err, result) => {
+            if (err) throw err;
+            comments = result;
+            totalComments = result.length
+        }
+    );
+
+    connection.end();
+
+
+    //create comment tree!
+    for (let i = 0; i < comments.length; i++) {
+        comments[i].replies = [];
+    }
+
+    for (let i = 0; i < comments.length; i++) {
+        if (comments[i].parentid) {
+            for (let j = 0; j < comments.length; j++) {
+                if (comments[j].commentid === comments[i].parentid) {
+                    comments[j].replies.push(comments[i]);
+                }
+            }
+        }
+    }
+
+    comments = comments.filter(comment => !comment.parentid);
+
+    return { comments, totalComments };
+}
+
+
+export const deleteComment = (commentid) => {
+    if (!commentid) {
+        throw new Error('Invalid comment parameters');
+    }
+
+    let connection = mysql.createConnection(sqlSettings());
+    createCommentsTable(connection);
+
+    connection.connect();
+
+    connection.query(
+        'UPDATE comments SET deleted = true WHERE commentid = ?',
+        [commentid],
+        (err, result) => {
+            if (err) throw err;
+            console.log('Comment deleted');
+        }
+    );
+
+    connection.end();
+
+    return true;
+}
+
+export const editComment = (commentid, content) => {
+    if (!commentid || !content) {
+        throw new Error('Invalid comment parameters');
+    }
+
+    let connection = mysql.createConnection(sqlSettings());
+    createCommentsTable(connection);
+    
+    connection.connect();
+
+    connection.query(
+        'UPDATE comments SET content = ? WHERE commentid = ?',
+        [content, commentid],
+        (err, result) => {
+            if (err) throw err;
+            console.log('Comment edited');
         }
     );
 
