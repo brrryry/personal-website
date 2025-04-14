970d920400b0be1a9149cf02a52c0609cd0676a8
brrryry <thisisbryanchan@gmail.com>
2025-04-14 12:54:21 -0400

    starting to write badminton blog

--- src/lib/likes.js
+++ src/lib/likes.js
@@ -2,117 +2,117 @@ import mysql from "mysql";
 import { sqlSettings } from "./sql";
 
 export const likeComment = (userid, commentid) => {
-    let connection = mysql.createConnection(sqlSettings());
-    connection.connect();
+  let connection = mysql.createConnection(sqlSettings());
+  connection.connect();
 
-    connection.query(
-        'SELECT * FROM comments WHERE commentid = ?',
-        [commentid],
-        (err, result) => {
-            if (err) throw err;
-            if (result.length === 0) {
-                throw new Error('Comment does not exist');
-            }
-        }
-    );
+  connection.query(
+    "SELECT * FROM comments WHERE commentid = ?",
+    [commentid],
+    (err, result) => {
+      if (err) throw err;
+      if (result.length === 0) {
+        throw new Error("Comment does not exist");
+      }
+    },
+  );
 
-    connection.query(
-        'SELECT * FROM accounts WHERE userid = ?',
-        [userid],
-        (err, result) => {
-            if (err) throw err;
-            if (result.length === 0) {
-                throw new Error('User does not exist');
-            }
-        }
-    );
+  connection.query(
+    "SELECT * FROM accounts WHERE userid = ?",
+    [userid],
+    (err, result) => {
+      if (err) throw err;
+      if (result.length === 0) {
+        throw new Error("User does not exist");
+      }
+    },
+  );
 
-    connection.query(
-        'SELECT * FROM likes WHERE userid = ? AND commentid = ?',
-        [userid, commentid],
-        (err, result) => {
-            if (err) throw err;
-            if (result.length > 0) {
-                throw new Error('User already liked this comment');
-            }
-        }
-    );
+  connection.query(
+    "SELECT * FROM likes WHERE userid = ? AND commentid = ?",
+    [userid, commentid],
+    (err, result) => {
+      if (err) throw err;
+      if (result.length > 0) {
+        throw new Error("User already liked this comment");
+      }
+    },
+  );
 
-    connection.query(
-        'INSERT INTO likes (userid, commentid) VALUES (?, ?)',
-        [userid, commentid],
-        (err) => {
-            if (err) throw err;
-        }
-    );
+  connection.query(
+    "INSERT INTO likes (userid, commentid) VALUES (?, ?)",
+    [userid, commentid],
+    (err) => {
+      if (err) throw err;
+    },
+  );
 
-    connection.end();
+  connection.end();
 
-    return true;
-}
+  return true;
+};
 
 export const unlikeComment = (userid, commentid) => {
-    let connection = mysql.createConnection(sqlSettings());
-    connection.connect();
+  let connection = mysql.createConnection(sqlSettings());
+  connection.connect();
 
-    connection.query(
-        'SELECT * FROM comments WHERE commentid = ?',
-        [commentid],
-        (err, result) => {
-            if (err) throw err;
-            if (result.length === 0) {
-                throw new Error('Comment does not exist');
-            }
-        }
-    );
+  connection.query(
+    "SELECT * FROM comments WHERE commentid = ?",
+    [commentid],
+    (err, result) => {
+      if (err) throw err;
+      if (result.length === 0) {
+        throw new Error("Comment does not exist");
+      }
+    },
+  );
 
-    connection.query(
-        'SELECT * FROM accounts WHERE userid = ?',
-        [userid],
-        (err, result) => {
-            if (err) throw err;
-            if (result.length === 0) {
-                throw new Error('User does not exist');
-            }
-        }
-    );
+  connection.query(
+    "SELECT * FROM accounts WHERE userid = ?",
+    [userid],
+    (err, result) => {
+      if (err) throw err;
+      if (result.length === 0) {
+        throw new Error("User does not exist");
+      }
+    },
+  );
 
-    connection.query(
-        'SELECT * FROM likes WHERE userid = ? AND commentid = ?',
-        [userid, commentid],
-        (err, result) => {
-            if (err) throw err;
-            if (result.length === 0) {
-                throw new Error('User has not liked this comment');
-            }
-        }
-    );
+  connection.query(
+    "SELECT * FROM likes WHERE userid = ? AND commentid = ?",
+    [userid, commentid],
+    (err, result) => {
+      if (err) throw err;
+      if (result.length === 0) {
+        throw new Error("User has not liked this comment");
+      }
+    },
+  );
 
-    connection.query(
-        'DELETE FROM likes WHERE userid = ? AND commentid = ?',
-        [userid, commentid],
-        (err) => {
-            if (err) throw err;
-        }
-    );
+  connection.query(
+    "DELETE FROM likes WHERE userid = ? AND commentid = ?",
+    [userid, commentid],
+    (err) => {
+      if (err) throw err;
+    },
+  );
 
-    connection.end();
+  connection.end();
 
-    return true;
-}
+  return true;
+};
 
 export const getLikes = (commentid) => {
-    let connection = mysql.createConnection(sqlSettings());
-    connection.connect();
+  let connection = mysql.createConnection(sqlSettings());
+  connection.connect();
 
-    connection.query(
-        'SELECT * FROM likes WHERE commentid = ?',
-        [commentid],
-        (err, result) => {
-            if (err) throw err;
-            return result;
-        }
-    );
+  connection.query(
+    "SELECT * FROM likes WHERE commentid = ?",
+    [commentid],
+    (err, result) => {
+      if (err) throw err;
+      return result;
+    },
+  );
 
-    connection.end();
-}
+  connection.end();
+};
