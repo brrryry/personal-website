232f8953b44e3b0db0318a6e12dbbbdb37a0f6b8
YouLikeCats <thisisbryanchan@gmail.com>
2025-07-10 21:14:01 -0400

    added new error checking to relevant files, still need to test

--- src/app/api/account/register/route.js
+++ src/app/api/account/register/route.js
@@ -1,22 +1,19 @@
 import { createNewAccount } from "@/lib/accounts";
 import { NextResponse } from "next/server";
 
+import { handleError, RouteError } from "@/lib/errors";
+
 export async function POST(req) {
     try {
         const { username, password } = await req.json();
-
-        // Validate input
-        if (!username || !password) {
-            return new Response(JSON.stringify({ error: "username and password are required" }), { status: 400 });
-        }
 
         // Create a new account
         const newAccount = await createNewAccount(username, password);
 
         // Return the newly created account details
-        return new Response(JSON.stringify(newAccount), { status: 201 });
+        return NextResponse.json(newAccount, { status: 201 });
     } catch (error) {
-        console.error("Registration error:", error);
-        return NextResponse.json({error: error.error || "registration failed"}, { status: error.status || 500 });
+        let err = RouteError.fromBaseError(error, "post /api/account/register");
+        return handleError(err);
     }
 }
