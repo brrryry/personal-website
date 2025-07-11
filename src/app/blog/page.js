d8b05e4de2d55ad9d0aebf764d5dac1dd74fa68c
YouLikeCats <thisisbryanchan@gmail.com>
2025-07-11 10:54:26 -0400

    refurbished blog architecture to allow nested directories (blog series stuff for ms-dp100)

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -8,15 +8,32 @@ export default async function Blog() {
   });
   allTags = [...new Set(allTags)];
   allTags = allTags.sort((a, b) => a.localeCompare(b));
+  allTags = allTags.filter((tag) => tag !== "series"); //remove "series" tag - should not be queryable
+
 
   return (
-    <div className="flex">
+    <div className="flex flex-col md:flex-row">
+      {/* Tags on top for small screens */}
+      <div className="block md:hidden mb-4">
+        <div className="text-left">
+          tags <br />
+          - - -
+          <div>
+            <a href="/blog">all</a>
+          </div>
+          {allTags.map((tag, i) => (
+            <div key={tag + i}>
+              <a href={`/blog/tag/${tag}`}>{tag}</a>
+            </div>
+          ))}
+        </div>
+        - - -
+      </div>
+      {/* Posts list */}
       <div className="space-y-5 md:w-4/5">
-        {/* Keep the existing code here */}
-        {/* Add this <section> tag below the existing <section> tag */}- - -
         <ul>
           {allPostsData.map((post) => {
-            if (post)
+            if (post && !post.data.tags.includes("series"))
               return (
                 <li key={post.id} className="py-2">
                   <p>
@@ -27,7 +44,7 @@ export default async function Blog() {
                     {post.data.tags.sort().map((tag, i) => {
                       return (
                         <a
-                          href={`/blog/tag/${tag.replaceAll}`}
+                          href={`/blog/tag/${tag}`}
                           key={post.id + tag}
                         >
                           {i < post.data.tags.length - 1 ? tag + ", " : tag}
@@ -42,21 +59,17 @@ export default async function Blog() {
           })}
         </ul>
       </div>
+      {/* Tags on right for medium+ screens */}
       <div className="justify-end text-right hidden md:block md:w-1/5">
-        - - -<br />
         tags
         <div>
           <a href="/blog">all</a>
-          <br />
         </div>
-        {allTags.map((tag, i) => {
-          return (
-            <div key={tag + i}>
-              <a href={`/blog/tag/${tag}`}>{tag}</a>
-              <br />
-            </div>
-          );
-        })}
+        {allTags.map((tag, i) => (
+          <div key={tag + i}>
+            <a href={`/blog/tag/${tag}`}>{tag}</a>
+          </div>
+        ))}
       </div>
     </div>
   );
