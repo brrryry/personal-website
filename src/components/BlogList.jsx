e8faf7a0ba531a24871f0b43b4346fea146e4ca7
brrryry <thisisbryanchan@gmail.com>
2026-06-15 15:36:21 -0400

    minor change with bloglist removed automatic <br> tags (i will manually use them from now on if necessary). new blog requires no <br> tags.

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,11 +1,7 @@
 export async function BlogList({ children }) {
   return (
-    <>
-      <br />
-      <div className="mx-6">
-        <ul className="list-disc">{children}</ul>
-      </div>
-      <br />
-    </>
+    <div className="mx-6">
+      <ul className="list-disc">{children}</ul>
+    </div>
   );
 }
